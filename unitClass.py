b855df3b42d63464eb480a26ce1b099dd40f0f55
Yarl <yarl_48ru@mail.ru>
2022-12-17 15:13:55 +0300

    Переделано

--- unitClass.py
+++ unitClass.py
@@ -1,4 +1,4 @@
-import json
+import json_tricks as json
 
 
 class Unit:
@@ -8,18 +8,6 @@ class Unit:
             self.hp = data[0]
             self.damage = data[1]
             self.range_of_attack = data[2]
-            self.range_of_feet = data[3]
+            self.range_of_movement = data[3]
             self.image = data[4]
             read_file.close()
-
-    def change_hp(self, damage_given):
-        self.hp -= damage_given
-
-    def give_damage(self):
-        return self.damage
-
-    def take_range_of_attack(self):
-        return self.range_of_attack
-
-    def take_range_of_feet(self):
-        return self.range_of_feet
