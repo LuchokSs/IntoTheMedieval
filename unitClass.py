6742460d6df78a1e2ed0e6c62834b078d6b1fd05
LuchokSs <sdomahnev@gmail.com>
2022-12-17 18:46:30 +0300

    Акция "помоги другу назвать переменные хорошо" прошла успешно.

--- unitClass.py
+++ unitClass.py
@@ -5,9 +5,9 @@ class Unit:
     def __init__(self, name_of_json_file):
         with open(name_of_json_file, "r") as read_file:
             data = json.load(read_file)
-            self.hp = data[0]
+            self.health = data[0]
             self.damage = data[1]
             self.range_of_attack = data[2]
-            self.range_of_movement = data[3]
+            self.movement_range = data[3]
             self.image = data[4]
             read_file.close()
