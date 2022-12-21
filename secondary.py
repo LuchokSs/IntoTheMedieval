d80b7df7f379938bb325d5e06c43ddd81f61380c
Yarl <yarl_48ru@mail.ru>
2022-12-21 14:07:47 +0300

    Запись переданного Unit в json файл в функции new_unit

--- secondary.py
+++ secondary.py
@@ -4,7 +4,11 @@ import sys
 
 import pygame
 
+import json_tricks as json
+
 from PIL import Image
+
+from globals import UNITS
 
 
 def load_image(name: str, colorkey=None) -> pygame.surface:
@@ -33,3 +37,11 @@ def pil_image_to_surface(image, direction=True):
         return Image.frombytes(str_format, image.get_size(), raw_str)
 
     return pygame.image.fromstring(image.tobytes(), image.size, image.mode).convert()
+
+
+def new_unit(name_of_unit, health, damage, range_of_attack, movement_range, image):
+    with open(f"{name_of_unit}.json", "w") as write_file:
+        data = [health, damage, range_of_attack, movement_range, image]
+        json.dump(data, write_file)
+        UNITS[name_of_unit] = f"{name_of_unit}.json"
+        write_file.close()
