d09488a3be1502b5a3f3d9d5c682742d74e5ee42
LuchokSs <sdomahnev@gmail.com>
2022-12-25 13:11:21 +0300

    Добавлена возможность юнитов использовать способности. Внесены небольшие изменения в прочие классы. Добавлена возможность наносить урон домам.

--- unitClass.py
+++ unitClass.py
@@ -1,5 +1,6 @@
 from secondary import load_image
-from globals import MOVEMENT_TYPES
+from globals import MOVEMENT_TYPES, HOUSE_DAMAGED_EVENT
+import pygame
 
 
 class Unit:
@@ -13,3 +14,30 @@ class Unit:
 
     def get_image(self):
         return load_image(self.image, colorkey='black')
+
+
+class Warrior(Unit):
+    def show_spellrange(self, field, pos):
+        field[pos[0] + 1][pos[1]].marked = True
+        field[pos[0]][pos[1] + 1].marked = True
+        field[pos[0] - 1][pos[1]].marked = True
+        field[pos[0]][pos[1] - 1].marked = True
+
+    def cast_spell(self, field, pos, unit):
+        event = HOUSE_DAMAGED_EVENT
+        event.pos = []
+        if pos.crds[0] == unit.crds[0]:
+            x, y = pos.crds
+            for dx in range(-1, 2, 1):
+                if field[x + dx][y].content is not None:
+                    field[x + dx][y].content.health -= 1
+                if field[x + dx][y].cell_type_id == 2:
+                    event.pos.append([x + dx, y])
+        elif pos.crds[1] == unit.crds[1]:
+            x, y = pos.crds
+            for dy in range(-1, 2, 1):
+                if field[x][y + dy].content is not None:
+                    field[x][y + dy].content.health -= 1
+                if field[x][y + dy].cell_type_id == 2:
+                    event.pos.append([x, y + dy])
+        pygame.event.post(event)
